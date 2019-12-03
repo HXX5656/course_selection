b0f343ddfaddd840115f457fbd875b7ad3383e3c
16302016001 <16302016001@fudan.edu.cn>
2019-12-03 20:07:43 +0900

    Student: change test order

--- src/test/main/DAO/StudentDAOImplTest.java
+++ src/test/main/DAO/StudentDAOImplTest.java
@@ -1,72 +1,67 @@
-package test.main.DAO; 
+package test.main.DAO;
 
 import main.DAO.DAOFactory;
 import main.DAO.StudentDAO;
 import main.entity.Student;
 import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After; 
+import org.junit.Before;
+import org.junit.After;
 
-/** 
-* StudentDAOImpl Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>12/03/2019</pre>
-* @version 1.0 
-*/ 
-public class StudentDAOImplTest { 
+/**
+ * StudentDAOImpl Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>12/03/2019</pre>
+ */
+public class StudentDAOImplTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-private static StudentDAO studentDAO = DAOFactory.getStudentDAOInstance();
-/** 
-* 
-* Method: append(Student student) 
-* 
-*/ 
-@Test
-public void testAppend() throws Exception {
-    Student student=new Student("1","blw",null,null,"1");
-    assert (studentDAO.append(student)==1);
-} 
+    private static StudentDAO studentDAO = DAOFactory.getStudentDAOInstance();
 
-/** 
-* 
-* Method: delete(String student_id) 
-* 
-*/ 
-@Test
-public void testDelete() throws Exception {
-    String student_id ="1";
-    assert (studentDAO.delete(student_id)==1);
-} 
+    /**
+     * Method: append(Student student)
+     */
+    @Test
+    public void testAppend() throws Exception {
+        Student student = new Student("1", "blw", null, null, "1");
+        assert (studentDAO.append(student) == 1);
+    }
 
-/** 
-* 
-* Method: modify(Student student) 
-* 
-*/ 
-@Test
-public void testModify() throws Exception {
-    Student student =new Student("1","hxx","2005-01-13",null,"1");
-    assert (studentDAO.modify(student)==1);
-} 
 
-/** 
-* 
-* Method: infoList(String student_id) 
-* 
-*/ 
-@Test
-public void testInfoList() throws Exception {
-    String student_id ="1";
-    System.out.println(studentDAO.infoList(student_id));
-}
+    /**
+     * Method: modify(Student student)
+     */
+    @Test
+    public void testModify() throws Exception {
+        Student student = new Student("1", "hxx", "2005-01-13", null, "1");
+        assert (studentDAO.modify(student) == 1);
+    }
+
+    /**
+     * Method: infoList(String student_id)
+     */
+    @Test
+    public void testInfoList() throws Exception {
+        String student_id = "1";
+        System.out.println(studentDAO.infoList(student_id));
+    }
+
+    /**
+     * Method: delete(String student_id)
+     */
+    @Test
+    public void testDelete() throws Exception {
+        String student_id = "1";
+        assert (studentDAO.delete(student_id) == 1);
+    }
+
 
 } 
